988426337267a637436c289f44faf525772b8736
Görkem Yağcı <gorkemyagci705@gmail.com>
2026-01-20 14:31:51 +0300

    threejs-journey

---
+++ 05-animations/src/script.js
@@ -0,0 +1,55 @@
+import * as THREE from 'three'
+import gsap from "gsap"
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+// Animate
+
+let time = Date.now();
+
+const clock = new THREE.Clock();
+
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 })
+gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 });
+gsap.to(mesh.position, { duration: 1, delay: 3, y: 2 });
+gsap.to(mesh.position, { duration: 1, delay: 4, y: -2 });
+gsap.to(mesh.position, { duration: 1, delay: 5, y: 0 });
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+    // mesh.position.x = Math.cos(elapsedTime);
+    // mesh.position.y = Math.sin(elapsedTime);
+    // camera.lookAt(mesh.position);
+    // mesh.rotation.y = elapsedTime;
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(tick);
+};
+
+tick();
